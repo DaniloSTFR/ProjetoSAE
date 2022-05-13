2322e36d5c878ca37ab74649bf76316037b8beaf
DaniloSTFR <danilo.santosfr@gmail.com>
2022-05-13 18:37:11 -0300

    Craição do midleware para receber os Errros

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import './database';
-import express, { request, response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors"
 import { router } from './routes';
 const cors = require('cors');
 const app = express();
@@ -18,6 +19,19 @@ mongoose.Promise = global.Promise;
 app.use(cors());
 app.use(express.json());
 app.use( router );
+
+app.use( (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if( err instanceof Error ){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+    })
+});
 
 
 app.listen(3333, () => console.log("Projeto SAE is running!")); 
